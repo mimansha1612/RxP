c088cae9d93a6795b54306e822de12a0a0562b4c
Saksham Agarwal <114224728+sakshamagarwalm2@users.noreply.github.com>
2025-02-24 23:12:40 +0530

    auth

--- project/src/components/Header.tsx
+++ project/src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
 import { Stethoscope, BookOpen } from "lucide-react";
 
 interface HeaderProps {
@@ -31,6 +32,10 @@ function Header({ onNewChat, onHome }: HeaderProps) {
           </div>
 
           <div className="flex items-center">
+            <SignedIn>
+              <UserButton/>
+            </SignedIn>
+            <SignedOut>
             <button onClick={onNewChat} className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
               <span className="absolute inset-0 overflow-hidden rounded-full">
                 <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
@@ -55,6 +60,7 @@ function Header({ onNewChat, onHome }: HeaderProps) {
               </div>
               <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
             </button>
+            </SignedOut>
           </div>
         </div>
       </div>
